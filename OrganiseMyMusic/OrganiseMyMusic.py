f7540c95720613131c15dabd6bf311447f15d245
rootavish <avishkar_gupta@outlook.com>
2015-02-09 23:40:10 +0530

    [#Wigglytuff] Music script done, does not delete empty directories as of yet.
    A possible way to implement this would be to delete all empty directories in the "dest" variable.
    Also no GUI and really crappy error messages. Needs more cleanup.

--- OrganiseMyMusic/OrganiseMyMusic.py
+++ OrganiseMyMusic/OrganiseMyMusic.py
@@ -1,48 +1,75 @@
 import platform
 import re
 import os
-
-separator = ""
+from id3py import ID3
+from id3py.ID3 import *
+from helpers import move, destdir
 
 # Decide the directory separator based on the OS
 # Also get the partitions to walk through.
 def getPartitions():
 
-	drive = []
+    global separator
+    drive = []
 
-	if platform.system() == "Windows":
-		separator = "\\"
-		drives = raw_input("Enter the partitions on your windows machine, separated by a space ")
+    if platform.system() == "Windows":
+        separator = "\\"
+        drives = raw_input("Enter the partitions or directories you'd like to search on your windows machine, separated by a space: ")
+        # #raw
+        print("\n")
 
-	 	drives = drives.split(' ')
+        drives = drives.split(' ')
 
-		for dr in drives:
-			if re.match('^[a-z]:\\\\$',dr,re.IGNORECASE):
-				drive.append(dr)
-			elif re.match("^[a-z]$",dr,re.IGNORECASE):
-				dr += ":\\"
-				drive.append(dr)
-			elif re.match("^[a-z]:$",dr,re.IGNORECASE):
-				dr += "\\"
-				drive.append(dr)
-			else:
-				print("Unrecognized option %s, skipping." % dr)
+        for dr in drives:
+            if re.match('^[a-z]:\\\\[a-z]+\\$',dr,re.IGNORECASE):
+                drive.append(dr)
+            elif re.match("^[a-z]$",dr,re.IGNORECASE):
+                dr += ":\\"
+                drive.append(dr)
+            elif re.match("^[a-z]:$",dr,re.IGNORECASE):
+                dr += "\\"
+                drive.append(dr)
+            else:
+                drive.append(dr)
+        
+        destdir()
+    else:
+        separator = "/"
+        # Linux is a TODO.
+        # SORRY.
 
-	else:
-		separator = "/"
-		# Linux is a TODO.
-		# SORRY.
+    return drive
 
-	return drive
+
+def sort(current, id3tags):
+    move(current, id3tags)
+#   print current, id3tags, "\n"
+
+def getID3(curfile):
+    id3tags = ID3(curfile).as_dict()
+    return id3tags
+
 
 # Get the partitions, and pass
 def musicScan(drives):
-	for drive in drives:
-		for root, dirs, files in os.walk(drive):
-# for all files in directory
-			for file in files:
-				print root + separator + file
+
+    for drive in drives:
+        
+        if os.path.isdir(drive) == False:
+            print "Could not find " + drive + ", skipping."
+            continue
+
+        for root, dirs, files in os.walk(drive):
+            # for all files in directory
+            for file in files:
+                curfile = root + separator + file
+
+                ext = os.path.splitext(curfile)[-1].lower()
+                
+                if ext == ".mp3":
+                    sort(curfile, getID3(curfile))
+
 
 # This is the main module we will use.
 def run():
-	musicScan(getPartitions())
+    musicScan(getPartitions())
---
+++ OrganiseMyMusic/helpers.py
@@ -0,0 +1,33 @@
+import shutil, os
+
+def move(source, id3tags):
+    # print id3tags
+    #print "moving to " +  + "from " + source
+    if (os.path.exists(source)):
+        if 'ALBUM' in id3tags and 'ARTIST' in id3tags:
+             
+             s = id3tags['ARTIST'] + "\\"+ id3tags['ALBUM']
+             dest = "".join(x for x in s if x.isalnum() or x == "\\" or x == " ")
+             dest = movedir + dest
+             if not os.path.isdir(dest):
+                os.makedirs(dest)
+
+             print dest
+             if os.path.isdir(dest):
+                shutil.move(source, dest)
+             else:
+                print "Could not create directories."
+
+        else:
+            print "skipping " + source + ", metadata not complete."
+
+
+def destdir():
+    global movedir
+    movedir = raw_input('Enter directory where you want to move all your music to: ')
+
+    if movedir[-1] != "\\":
+        movedir += "\\"
+
+    if not os.path.exists(movedir):
+        os.makedirs(movedir)
